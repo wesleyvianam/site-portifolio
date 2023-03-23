35ee777b24c2642f2522639f14024d4948430531
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-03-23 05:25:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Ola mundo!
+    </div>
+  )
+}
+
+export default App
